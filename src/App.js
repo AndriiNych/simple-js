3b6fa2e6923fdbdd093d84c28de4aa5bbe503029
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2024-04-09 13:05:54 +0300

    returned the function of creating routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 
 import AppMainWindow from 'interface/windows/AppMainWindow';
 import { Page404 } from 'pages';
-// import { getRoutesFromProjectList } from 'data';
-
-import { HomePageDetails } from 'pages';
-import { ImageGalleryDetails, ImageGalleryProject } from 'projects';
+import { getRoutesFromProjectList } from 'data';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<AppMainWindow />}>
-        <Route index element={<HomePageDetails />} />
-        <Route path="ImageGalleryDetails" element={<ImageGalleryDetails />} />
-        <Route path="ImageGalleryProject" element={<ImageGalleryProject />} />
-        {/* {getRoutesFromProjectList()} */}
+        {getRoutesFromProjectList()}
         <Route path="*" element={<Page404 />} />
       </Route>
     </Routes>
