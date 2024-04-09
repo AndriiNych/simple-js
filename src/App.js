52ed42b79d026d81ff2dfc3fd3f7be0d48598c08
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2024-04-09 10:11:51 +0300

    test route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import { Routes, Route } from 'react-router-dom';
 
 import AppMainWindow from 'interface/windows/AppMainWindow';
 import { Page404 } from 'pages';
-import { getRoutesFromProjectList } from 'data';
+// import { getRoutesFromProjectList } from 'data';
+
+import { HomePageDetails } from 'pages';
+import { ImageGalleryDetails, ImageGalleryProject } from 'projects';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<AppMainWindow />}>
-        {getRoutesFromProjectList()}
+        <Route index element={<HomePageDetails />} />
+        <Route
+          path="/image-gallery-details"
+          element={<ImageGalleryDetails />}
+        />
+        <Route
+          path="/image-gallery-project"
+          element={<ImageGalleryProject />}
+        />
+        {/* {getRoutesFromProjectList()} */}
         <Route path="*" element={<Page404 />} />
       </Route>
     </Routes>
